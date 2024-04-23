55f661c6cef4bdde1b99d900ffc653e1a439fa44
krishnas05 <kanavkrishna05@gmail.com>
2024-04-23 12:22:44 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,34 @@
+// c:/Users/GR0012AU/OneDrive/Desktop/miniProject/project/src/app/page.js
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Hello World!</div>
+    </main>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// I am working on a project in which i have to make a user interface in website in which we will be receiving data from the iot device on the smart dustbins about the details like percentage of how much level dustbin is filled. SO i have to show thus data ere will be various connected dustbins in such a way that there will be various connected dustbins showm on the screen with a card view 
